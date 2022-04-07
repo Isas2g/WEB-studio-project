fa9ff5980297e40dedeebd6a7f1fd823f5cd0ec3
Vasiliy Poperekov <vasya.poperekov@mail.ru>
2022-04-07 16:16:50 +0300

    Were implemented all models from ER-diagram

--- WebStudio/resources/models.py
+++ WebStudio/resources/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Model, CharField
+from django.db import models
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db.models import Model, CharField, ImageField
 
