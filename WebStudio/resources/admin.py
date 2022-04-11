901a84b5da5d65b7af08940854da2790ff2e94f4
DeNeMiX83 <daniil53891@yandex.ru>
2022-04-11 16:07:56 +0300

    fix DB models + docker

--- WebStudio/resources/admin.py
+++ WebStudio/resources/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from resources.models import *
+
+from resources.models import HumanContacts
+
+# admin.site.register(CustomUsers, UserAdmin)
+# admin.site.register(Posts, UserAdmin)
+# admin.site.register(HumanContacts, UserAdmin)
+# admin.site.register(Posts, UserAdmin)
