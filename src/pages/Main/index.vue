e15ca227c1fbf750cb7f76d3a1e919c4efbd74f0
Renat <parabell241@gmail.com>
2022-04-20 21:38:13 +0300

    started first block for desktop

--- src/pages/Main/index.vue
+++ src/pages/Main/index.vue
@@ -15,9 +15,9 @@
 
     <main>
       <section class="main-block">
-        <h1>Студенческая веб-студия Московского Политеха</h1>
-        <p>Мы крутые ребята из Московского политеха и создаем крутой контент для тебя и всех остальных</p>
-        <button>Подробнее</button>
+        <h1 class="title">Студенческая веб-студия Московского Политеха</h1>
+        <p class="subtitle">Мы крутые ребята из Московского политеха и создаем крутой контент для тебя и всех остальных</p>
+        <button class="more-btn">Подробнее</button>
       </section>
     </main>
 
@@ -31,15 +31,11 @@
   };
 </script>
 
-<style lang="scss">
-  body {
-    background: #000;
-    color: #fff;
-  }
+<style scoped lang="scss">
 
   .container {
     max-width: 1200px;
-    padding: 15px;
+    padding: 40px 15px;
     margin: 0 auto;
     font-family: 'inter';
   }
@@ -70,6 +66,36 @@
 
   .main-block {
     padding: 150px 0 150px 0;
+  }
+  
+  .title {
+    background-image: linear-gradient(225deg, #F70193, #A72CF2);
+    filter: drop-shadow(0 20px 30px #28d8ff33);
+    color: black;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    box-decoration-break: clone;
+    -webkit-box-decoration-break: clone;
+    font-size: 50px;
+    max-width: 670px;
+    margin-bottom: 30px;
+  }
+
+  .subtitle {
+    margin-bottom: 30px;
+    max-width: 440px
+  }
+
+  .more-btn {
+    background: linear-gradient(95.68deg, #FF0000 -16.11%, #FF008A 54.53%);
+    border-radius: 5px;
+    padding: 10px 50px;
+    border: none;
+    color: #fff;
+    font-family: 'Inter';
+    font-weight: 700;
+    font-size: 16px;
   }
 
 </style>
