c1dc9559d2774f4b9dee296fe3242ee09f6331e6
Denis Kamchatov <kamcatovdenis@gmail.com>
2022-04-22 23:36:40 +0300

    Sketches of AboutPage and uploading of team images

--- src/pages/About/AboutPage.vue
+++ src/pages/About/AboutPage.vue
@@ -1,7 +1,106 @@
 <template>
   <main class="main about">
-    <section class="about__team about-team"></section>
-    <section class="about__projects about-projects"></section>
+    <section class="about__team about-team">
+      <h2 class="about-team__title">Наша команда</h2>
+
+      <div class="about-team__direction direction">
+        <h3 class="direction__title">Менеджеры</h3>
+        <ul class="direction__members">
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Настя</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Ренат</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Влад</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+        </ul>
+      </div>
+
+      <div class="about-team__direction direction">
+        <h3 class="direction__title">Бэкенд</h3>
+        <ul class="direction__members">
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Илья</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Данил</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Вася</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+        </ul>
+      </div>
+
+      <div class="about-team__direction direction">
+        <h3 class="direction__title">Фронтенд</h3>
+        <ul class="direction__members">
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Денис</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Софа</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+        </ul>
+      </div>
+
+      <div class="about-team__direction direction">
+        <h3 class="direction__title">Дизайнеры</h3>
+        <ul class="direction__members">
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Алёна</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+          <li class="direction__member">
+            <img class="direction__member-image" src="" alt="" />
+            <h4 class="direction__mamber-name">Алёна</h4>
+            <h5 class="direction__mamber-description">
+              А тут описание под заголовком
+            </h5>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="about__projects about-projects">
+      <h2 class="about-projects__title">Наши проекты</h2>
+    </section>
   </main>
 </template>
 
