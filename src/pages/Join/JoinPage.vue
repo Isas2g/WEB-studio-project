b956ef2e135e0f0d732cab146a7c6f48a9245d71
Denis Kamchatov <kamcatovdenis@gmail.com>
2022-04-25 00:01:25 +0300

    created JoinPage

---
+++ src/pages/Join/JoinPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <main class="join container">
+    <section class="join__looking looking">
+      <h2 class="looking__title">Кого мы ищем:</h2>
+      <ul class="looking__persons">
+        <li class="looking__person">
+          <img
+            class="looking__person-icon"
+            src="../../assets/images/person-icon.svg"
+            alt="Персона"
+          />
+          <div class="looking__person-info">
+            <h4 class="looking__person-post">кого-то</h4>
+            <h5 class="looking__person-skills">навыки</h5>
+          </div>
+        </li>
+        <li class="looking__person">
+          <img
+            class="looking__person-icon"
+            src="../../assets/images/person-icon.svg"
+            alt="Персона"
+          />
+          <div class="looking__person-info">
+            <h4 class="looking__person-post">кого-то</h4>
+            <h5 class="looking__person-skills">навыки</h5>
+          </div>
+        </li>
+        <li class="looking__person">
+          <img
+            class="looking__person-icon"
+            src="../../assets/images/person-icon.svg"
+            alt="Персона"
+          />
+          <div class="looking__person-info">
+            <h4 class="looking__person-post">кого-то</h4>
+            <h5 class="looking__person-skills">навыки</h5>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.looking__persons {
+  display: flex;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MainPage from "../pages/Main/MainPage.vue";
 import AboutPage from "../pages/About/AboutPage.vue";
+import JoinPage from "../pages/Join/JoinPage.vue";
 
 
 Vue.use(VueRouter)
@@ -16,6 +17,11 @@ const routes = [
         path: '/about',
         name: 'ApoutPage',
         component: AboutPage
+    },
+    {
+        path: '/join',
+        name: 'JoinPage',
+        component: JoinPage
     },
 ]
 
