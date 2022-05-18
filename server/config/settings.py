1188a92444fa9c5f2e27b481eeac031e921f3e65
Vasiliy Poperekov <vasya.poperekov@mail.ru>
2022-05-18 20:34:20 +0300

    Was implemented pagination and searching in projects app

--- server/config/settings.py
+++ server/config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'src.users',
     'src.boards',
     'src.projects',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
+    'PAGINATE_BY_PARAM': 'page_size'
+}
